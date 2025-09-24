143a376fdeb2671cb81fea7ccae8967ea0e28283
Aniruddh Achar <aniruddhachars@gmail.com>
2025-09-24 12:22:16 +0530

    Time: 4 ms (99.68%), Space: 45.4 MB (61.86%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,25 +1,18 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        int start = 0;
-        int end = s.length() - 1;
-
+        return palindromeDelete(s, 0, s.length() - 1, false);
+    }
+    public boolean palindromeDelete(String s, int start, int end, boolean delete) {
         while(start < end) {
             if(s.charAt(start) == s.charAt(end)) {
                 start++;
                 end--;
             } else {
-                return palindromeRange(s, start + 1, end) || palindromeRange(s, start, end - 1);
+                if(delete) {
+                    return false;
+                }
+                return palindromeDelete(s, start + 1, end, true) || palindromeDelete(s, start, end - 1, true);
             }
-        }
-        return true;
-    }
-    public boolean palindromeRange(String s, int start, int end) {
-        while(start < end) {
-            if(s.charAt(start) != s.charAt(end)) {
-                return false;
-            }
-            start++;
-            end--;
         }
         return true;
     }
