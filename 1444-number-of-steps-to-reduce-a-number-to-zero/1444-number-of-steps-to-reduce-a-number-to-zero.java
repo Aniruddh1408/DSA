cc5c846823c357b6f44d18964f69fe341e0ec12e
Aniruddh Achar <aniruddhachars@gmail.com>
2025-10-24 11:55:09 +0530

    Time: 0 ms (100%), Space: 40.9 MB (11.58%) - LeetHub

--- 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.java
+++ 1444-number-of-steps-to-reduce-a-number-to-zero/1444-number-of-steps-to-reduce-a-number-to-zero.java
@@ -1,14 +1,15 @@
 class Solution {
     public int numberOfSteps(int num) {
-        int steps = 0;
-        while(num > 0) {
-            if(num % 2 == 0) {
-                num /= 2;
-            } else {
-                num -= 1;
-            }
-            steps++;
+       return helper(num, 0);
+    }
+    int helper(int num, int steps) {
+        if(num == 0) {
+            return steps;
         }
-        return steps;
+        if(num % 2 == 0) {
+            return helper(num / 2, steps + 1);
+        } else {
+            return helper(num - 1, steps + 1);
+        }
     }
 }
