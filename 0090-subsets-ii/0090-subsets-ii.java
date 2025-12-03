cd1b52b81fb49be0a5704caf49282ebf1bc6cf8d
Aniruddh Achar <aniruddhachars@gmail.com>
2025-12-04 00:41:38 +0530

    Time: 2 ms (62.74%), Space: 44.6 MB (51.01%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -5,18 +5,15 @@ class Solution {
         backtrack(0, nums, ans, new ArrayList<>());
         return ans;
     }
-    public void backtrack(int start, int[] nums, List<List<Integer>> res, List<Integer> cur) {
-        if(start == nums.length) {
-            res.add(new ArrayList<>(cur));
-            return;
+    public void backtrack(int start, int nums[], List<List<Integer>> res, List<Integer> cur) {
+        res.add(new ArrayList<>(cur));
+        for(int i = start; i < nums.length; i++) {
+            if(i > start && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            cur.add(nums[i]);
+            backtrack(i + 1, nums, res, cur);
+            cur.remove(cur.size() - 1);
         }
-        cur.add(nums[start]);
-        backtrack(start + 1, nums, res, cur);
-        cur.remove(cur.size() - 1);
-         
-         while (start + 1 < nums.length && nums[start] == nums[start + 1]) {
-            start++;
-        }
-        backtrack(start + 1, nums, res, cur);
     }
 }
